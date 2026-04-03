bf09b70eb11960690f70cbc5a6b8f9328963b945
Matteo Prendi <mateoprendi@proton.me>
2026-04-03 21:16:55 +0200

    feat: add inter & merriweather fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
@@ -12,4 +10,19 @@ export default defineConfig({
     },
 
     integrations: [react()],
+
+    fonts: [
+        {
+            name: "Inter",
+            cssVariable: "--font-sans",
+            provider: fontProviders.google(),
+            weights: ["100 900"],
+        },
+        {
+            name: "Merriweather",
+            cssVariable: "--font-serif",
+            provider: fontProviders.google(),
+            weights: ["300 900"],
+        },
+    ],
 });
