ab1e02c97911f9328a7b0cee420a4e84b22b1291
Matteo Prendi <mateoprendi@proton.me>
2026-06-19 15:04:22 +0200

    feat: add persian fonts

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,13 +1,30 @@
-import { Inter, Merriweather } from "next/font/google";
+import {
+  Inter,
+  Merriweather,
+  Noto_Naskh_Arabic,
+  Vazirmatn,
+} from "next/font/google";
 import "@/app/(website)/global.css";
 
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 
 import { getCurrentLocale } from "@/utils/locale";
+import React from "react";
 
-const sans = Inter({ variable: "--font-sans", subsets: ["latin"] });
-const serif = Merriweather({ variable: "--font-serif", subsets: ["latin"] });
+const latinSans = Inter({ variable: "--latin-sans", subsets: ["latin"] });
+const latinSerif = Merriweather({
+  variable: "--latin-serif",
+  subsets: ["latin"],
+});
+const persianSerif = Noto_Naskh_Arabic({
+  variable: "--persian-serif",
+  subsets: ["arabic"],
+});
+const persianSans = Vazirmatn({
+  variable: "--persian-sans",
+  subsets: ["arabic"],
+});
 
 interface Props {
   readonly children: React.ReactNode;
@@ -15,9 +32,25 @@ interface Props {
 
 export default async function RootLayout({ children }: Props) {
   const lang = await getCurrentLocale();
+  const isFarsi = lang === "fa";
+
+  const activeSans = isFarsi ? persianSans : latinSans;
+  const activeSerif = isFarsi ? persianSerif : latinSerif;
 
   return (
-    <html lang={lang} className={`${sans.variable} ${serif.variable}`}>
+    <html
+      lang={lang}
+      dir={isFarsi ? "rtl" : "ltr"}
+      className={`${activeSans.variable} ${activeSerif.variable}`}
+      style={
+        {
+          "--font-sans": isFarsi ? "var(--persian-sans)" : "var(--latin-sans)",
+          "--font-serif": isFarsi
+            ? "var(--persian-serif)"
+            : "var(--latin-serif)",
+        } as React.CSSProperties
+      }
+    >
       <body className="text-foreground bg-background">
         <Header />
         {children}
