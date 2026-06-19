44000722f57d2191784e04f22835ca12600470ff
Matteo Prendi <mateoprendi@proton.me>
2026-06-19 13:01:34 +0200

    fix: stop middleware from running in admin

--- middleware.ts
+++ middleware.ts
@@ -31,5 +31,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|images|api).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|images|api|admin).*)"],
 };
